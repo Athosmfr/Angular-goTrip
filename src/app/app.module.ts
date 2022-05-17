be665d2f64dd7c54592e760ed79c1f01941b8c96
AthosMFR <athos.mfrodrigues@gmail.com>
2022-05-17 01:29:52 -0300

    testeUpload

---
+++ src/app/app.module.ts
@@ -0,0 +1,72 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { NavComponent } from './nav/nav.component';
+import { AdminComponent } from './admin/admin.component';
+import { LoginComponent } from './login/login.component';
+import { AtualizarComponent } from './admin/atualizar/atualizar.component';
+import { ListarComponent } from './admin/listar/listar.component';
+import { ExcluirComponent } from './admin/excluir/excluir.component';
+import { IncluirComponent } from './admin/incluir/incluir.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+//Material Components
+import { MatSliderModule } from '@angular/material/slider';
+import { MatTableModule } from '@angular/material/table';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatInputModule } from '@angular/material/input'
+import { MatNativeDateModule } from '@angular/material/core';
+import { MatIconModule } from '@angular/material/icon';
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonToggleModule } from '@angular/material/button-toggle';
+
+import { HttpClientModule } from "@angular/common/http";
+import { PesquisaComponent } from './pesquisa/pesquisa.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    HeaderComponent,
+    FooterComponent,
+    NavComponent,
+    AdminComponent,
+    LoginComponent,
+    AtualizarComponent,
+    ListarComponent,
+    ExcluirComponent,
+    IncluirComponent,
+    PesquisaComponent
+  ],
+  imports: [
+    MatDatepickerModule,
+    MatFormFieldModule,
+    MatIconModule,
+    MatNativeDateModule,
+    MatInputModule,
+    MatAutocompleteModule,
+    MatSliderModule,
+    MatTableModule,
+    MatIconModule,
+    MatCardModule,
+    MatButtonToggleModule,
+    FormsModule,
+    ReactiveFormsModule,
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    HttpClientModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
